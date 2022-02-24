5fb6c936cd7135b7b83ebdd4f12ebad330437f5e
Lauren Kelz <s015459@students.lmsd.org>
2022-02-24 14:30:21 -0500

    Initial Commit

---
+++ Present/PresentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentApp.swift
+//  Present
+//
+//  Created by Lauren Kelz (student LM) on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct PresentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
