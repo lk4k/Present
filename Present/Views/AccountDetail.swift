10959128199dea5c2dac9f34dc42265df201396a
Inbal David <s016644@students.lmsd.org>
2022-03-30 13:47:25 -0400

    working copy

--- Present/Views/AccountDetail.swift
+++ Present/Views/AccountDetail.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct AccountDetail: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    class AccountDetail : ObservableObject{
+        var name : String
+        var email : String
+        var password : String
     }
 }
 
