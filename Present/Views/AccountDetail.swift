21e418a549a1be2c13f9b8e343f3aa53d84459ca
Lauren Kelz <s015459@students.lmsd.org>
2022-04-04 12:27:13 -0400

    account detail removed incorrect "fun facts"

--- Present/Views/AccountDetail.swift
+++ Present/Views/AccountDetail.swift
@@ -17,27 +17,27 @@ struct AccountDetail: View {
         
         //list of responses from parsed JSON information given the user's birthday
         VStack{
-            List(fetchData.responses.births) {
-                birth in
-                HStack{
-                    VStack{
-                        //Displays the name of the notable person
-                        Text("Name: \(birth.text ?? "")")
-                        
-                        //Displays the age of the notable person
-                        Text("Age: \(2022 - (birth.year ?? 2022))")
-                    }
-                    
-                    //List displays a photo of the notable person
-//                    KFImage(URL(string: birth.pages[0]?.thumbnail?.source ?? "") ?? URL(string :"https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg"))
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//                        .frame(width: 100, height: 50, alignment: .center)
-//                        .cornerRadius(10.0)
-                }
-                .cornerRadius(5.0)
-                
-            }.foregroundColor(Color.offWhite)
+//            List(fetchData.responses.births) {
+//                birth in
+//                HStack{
+//                    VStack{
+//                        //Displays the name of the notable person
+//                        Text("Name: \(birth.text ?? "")")
+//
+//                        //Displays the age of the notable person
+//                        Text("Age: \(2022 - (birth.year ?? 2022))")
+//                    }
+//
+//                    //List displays a photo of the notable person
+////                    KFImage(URL(string: birth.pages[0]?.thumbnail?.source ?? "") ?? URL(string :"https://cdn.mos.cms.futurecdn.net/PuXipAW3AXUzUJ4uYyxPKC-1200-80.jpg"))
+////                        .resizable()
+////                        .aspectRatio(contentMode: .fit)
+////                        .frame(width: 100, height: 50, alignment: .center)
+////                        .cornerRadius(10.0)
+//                }
+//                .cornerRadius(5.0)
+//
+//            }.foregroundColor(Color.offWhite)
             
             //displaying variables
             HStack{
