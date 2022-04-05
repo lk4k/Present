f6fed2c43ea438882100e1d08079990f227d543f
Ella Kesler <evkesler10@gmail.com>
2022-04-05 12:59:20 -0400

    Added friend info

---
+++ Present/Views/FriendView.swift
@@ -0,0 +1,22 @@
+//
+//  FriendView.swift
+//  Present
+//
+//  Created by Ella Kesler (student LM) on 4/4/22.
+//
+
+import SwiftUI
+
+class friendInfo : ObservableObject{
+    var name : String
+    @Published var birthday : Date
+    @Published var wishlist : String
+    @Published var image: UIImage = UIImage(named: "user")!
+    
+    init(name : String = "", birthday: Date = Date(), wishlist : String = ""){
+        self.name = name
+        self.birthday = birthday
+        self.wishlist = wishlist
+      
+    }
+}
