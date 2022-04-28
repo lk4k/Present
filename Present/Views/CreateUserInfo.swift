063c542914477c36c2a778f408ba7e8845e0c129
Lauren Kelz <s015459@students.lmsd.org>
2022-04-27 22:33:59 -0400

    create account fixed

--- Present/Views/CreateUserInfo.swift
+++ Present/Views/CreateUserInfo.swift
@@ -11,7 +11,7 @@ struct CreateUserInfo: View {
     @EnvironmentObject var userInfo : UserInfo
     
     //@StateObject var userInfo: UserInfo = UserInfo()
-
+    
     
     var body: some View {
         ZStack {
@@ -22,12 +22,12 @@ struct CreateUserInfo: View {
                 
                 Spacer()
                 
-//                Image(systemName: "person.crop.circle")
-//                    .resizable()
-//                    .foregroundColor(Color.Pink)
-//                    .frame(width: 200, height: 200)
-//                    .aspectRatio(contentMode: .fill)
-//                    .clipShape(Circle())
+                //                Image(systemName: "person.crop.circle")
+                //                    .resizable()
+                //                    .foregroundColor(Color.Pink)
+                //                    .frame(width: 200, height: 200)
+                //                    .aspectRatio(contentMode: .fill)
+                //                    .clipShape(Circle())
                 
                 Spacer()
                 VStack {
@@ -49,19 +49,30 @@ struct CreateUserInfo: View {
                         .foregroundColor(Color.black)
                         .font(Font.custom("Nunito-ExtraLight", size: 28))
                         .padding()
-                   
-        
+                    
+                    
                     //add wishlish
                     TextField("Wishlist", text: $userInfo.wishlist)
                         .disableAutocorrection(false)
                         .font(Font.custom("Nunito-ExtraLight", size: 22))
                         .padding()
                     
+                    Button("create account"){
+                        FirebaseFunctions.authenticate(email: userInfo.email, password: userInfo.password){ success in
+                            if success{
+                                userInfo.loggedIn = true
+                            }
+                        }
+                    }.frame(width: 150, height: 70)
+                    .background(Color.Pink)
+                    .foregroundColor(Color.black)
+                    .cornerRadius(80.0)
+                    .font(Font.custom("Nunito-ExtraLight", size: 30))
                     
                     
                 }.foregroundColor(Color.black)
                 .padding()
-
+                
                 Spacer()
                 Spacer()
                 Spacer()
